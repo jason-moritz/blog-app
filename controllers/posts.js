f8086fcfe92e79aaa03c7fe4ef0c2fa8d79e3724
digallardox <digallardox@gmail.com>
2021-10-04 13:57:19 -0500

    seed data.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,19 @@
-import Posts from '../models/post.js'
+import Post from '../models/post.js'
+
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: error.message })
+    }
+}
+
+// export const getPost = async (req, res) => {
+//     try {
+
+//     } catch () {
+        
+//     }
+// }
